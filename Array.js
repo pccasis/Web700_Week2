8d3dda1402c6af3308e6a0d023401f81817b84c3
Plinky Asis <Plinky Asis>
2023-05-17 16:45:38 -0400

    added array

---
+++ Array.js
@@ -0,0 +1,17 @@
+var a= [10, 20, 30, 40]
+var b = new Array ()
+console.log(typeof(a))
+
+console.log(a)
+
+console.log(a[0])
+console.log(a[2])
+console.log(a[4])
+
+a[5] = 500
+console.log(a[0])
+console.log(a)
+
+a.push(600)
+console.log(a)
+console.log(a.length)
