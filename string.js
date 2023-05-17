b41d7a0a8f5fcb4fe54987be2ada0160c1c3ef24
Plinky Asis <Plinky Asis>
2023-05-17 16:56:49 -0400

    add string

---
+++ string.js
@@ -0,0 +1,19 @@
+var college = "Seneca -BTT"
+var name = new string ("Plinky Asis")
+
+var a = college + name
+console.log(`${college}`)
+console.log(`${name}`)
+console.log(`${a}`)
+
+
+console.log (`Length: ${college.length}`)
+console.log (`Upper: ${college.toUpperCase()}`)
+console.log (`Lower: ${name.toLowerCase()}`)
+console.log (`IndexOf: ${college.indexOf("College")}`)
+console.log (`IndexOf: ${college.indexOf("hello")}`)
+console.log (`endsWith: ${college.endsWith("BTT")}`)
+console.log (`startsWith: ${college.startsWith("Seneca")}`)
+console.log (`includes: ${college.includes("College")}`)
+
+var s = college.split(' ')
